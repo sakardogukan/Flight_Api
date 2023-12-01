9bbcd8cbe1b6aa0452ef146926f25c9c0ca88a3d
DGKN <sakardogukan@gmail.com>
2023-12-01 23:52:58 +0300

    reservation.js route file added

---
+++ src/routes/reservation.js
@@ -0,0 +1,22 @@
+"use strict"
+/* -------------- REZERVATION ROUTE ------------------ */
+
+const router = require('express').Router()
+const reservation = require('../controllers/reservation')
+const permissions = require('../middlewares/permissions')
+
+router.use(permissions.isStaffOrAdmin)
+
+router.route('/')
+    .get(reservation.list)
+    .post(reservation.create)
+    
+router.route('/:id')
+    .get(reservation.read)
+    .put(reservation.update)
+    .patch(reservation.update)
+    .delete(permissions.isAdmin, reservation.delete)
+
+router.get('/:id/passengers', reservation.passengers)
+
+module.exports = router
