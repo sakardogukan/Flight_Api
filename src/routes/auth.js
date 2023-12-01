c17533e130edac9e1db2590d55f65837b444ac6b
DGKN <sakardogukan@gmail.com>
2023-12-01 23:54:11 +0300

    auth.js route file added

---
+++ src/routes/auth.js
@@ -0,0 +1,11 @@
+"use strict"
+/* ------------------- AUTHORIZATION ----------------- */
+const router = require('express').Router()
+
+const auth = require('../controllers/auth')
+
+router.post('/login', auth.login)
+router.post('/refresh', auth.refresh)
+router.get('/logout', auth.logout)
+
+module.exports = router
